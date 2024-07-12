96aacf71951575ad0a4eea95d2fb5b3dc494d6a5
ifelipesilva <ifelipesilvapro@gmail.com>
2024-07-12 17:17:32 -0300

    chore: Add user input functionality to create a new bank account

--- Projetos/ContaBanco/src/ContaTerminal.java
+++ Projetos/ContaBanco/src/ContaTerminal.java
@@ -1,11 +1,34 @@
-
+import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         
+        Scanner sc = new Scanner(System.in);
 
 
-        System.out.println("test");
+       System.out.println("Qual seu nome?");
+       String nome = sc.nextLine();
+
+
+       System.out.println("Qual seu sobrenome?");
+       String sobrenome = sc.nextLine();
+
+       System.out.println("Por favor, digite o número da conta !");
+
+       int conta = sc.nextInt();
+
+       System.out.println("Por favor, digite o número da Agência !");
+       int agencia = sc.nextInt();
+        
+       System.out.println("Por favor, digite o saldo da conta !");
+       double saldo = sc.nextDouble();
+
+
+
+
+        System.out.println("Ola, " + nome + " " + sobrenome + ", obrigado por criar uma conta em nosso banco, sua conta criada foi: " + conta + ", agencia: " + agencia + ", e seu saldo inicial foi: " + saldo + " Reais");
+
+        sc.close();
 
 
 
