9e8adf5730b03c72527ed8e611ce9e80e7492d01
ifelipesilva <ifelipesilvapro@gmail.com>
2024-07-12 16:12:10 -0300

    chore: Add variable declarations and print statements to Operators class

---
+++ types-variables/src/Operators.java
@@ -0,0 +1,17 @@
+public class Operators {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 3;
+    int z = x + y;
+    System.out.println(z);
+
+
+    int a = 5;
+    int b = 3;
+    int c = a - b;
+    System.out.println(c);  
+
+
+
+  }
+}
